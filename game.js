945c3d534b5f522b9a8650b440849346c72bbe44
piyashah123 <82143407+piyashah123@users.noreply.github.com>
2024-07-15 22:51:52 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,19 @@
+var gameOver=false
+$(".clickable").click(function (){
+    var correctGuess = false;
+    let id = $(this).attr("id");
+    var life = parseInt($("life").text())
+    for (var i =0; i< randomWord.word.lenght; i++){
+        if (randomWord.wordcharAt(i).toLowerCase() == id){
+            if (life > 0 && ($(".fill_blanks").eq(i).html()=="_" || $(".fill_blanks").eq(i).html()== id)){
+                $(".fill_blanks").eq(i).html(id);
+                correctGuess = true;
+                if($("#blanks").text() === randomWord.word.toLowerCase()) {
+                    $("#result").text("you win!!")
+                    correctGuess = true;
+                    gameOver = true
+                }
+            }
+        }
+    }
+})
